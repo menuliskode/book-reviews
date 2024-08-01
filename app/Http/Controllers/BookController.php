ed76f9c5eaff286d483732738bfdc20a33f160f3
menuliskode <menuliskode@gmail.com>
2024-08-01 17:29:00 +0800

    add more query on Book model and component star rating

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,8 +13,29 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
-        $books = Book::when($title, fn ($query, $title) => $query->title($title))->get();
+        $books = Book::when(
+            $title,
+            fn ($query, $title) => $query->title($title)
+        );
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()->withAvgRating()->withReviewsCount()
+        };
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books =
+            cache()->remember(
+                $cacheKey,
+                3600,
+                fn () =>
+                $books->get()
+            );
 
         return view('books.index', ['books' => $books]);
     }
